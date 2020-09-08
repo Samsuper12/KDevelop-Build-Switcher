d2aad75d91314334f0c5399706883e11daeab624
Michael Skorokhodov <bakaprogramm29@gmail.com>
2020-09-08 20:17:55 +0300

    Add QMenu example for context menu

--- src/build_switcher.cpp
+++ src/build_switcher.cpp
@@ -9,7 +9,7 @@
 //QT5
 #include <QAction>
 #include <QDebug>
-
+#include <QMenu>
 
 K_PLUGIN_FACTORY_WITH_JSON(Build_SwitcherFactory, "build_switcher.json", registerPlugin<Build_Switcher>(); )
 
@@ -17,19 +17,43 @@ Build_Switcher::Build_Switcher(QObject *parent, const QVariantList& args)
     : KDevelop::IPlugin(QStringLiteral("build_switcher"), parent)
 {
     Q_UNUSED(args);
-
+    
     //qCDebug(PLUGIN_BUILD_SWITCHER) << "Hello world, my plugin is loaded!";
+    
 }
 
 KDevelop::ContextMenuExtension Build_Switcher::contextMenuExtension(KDevelop::Context* context, QWidget* parent) {
     
     KDevelop::ContextMenuExtension ext = KDevelop::IPlugin::contextMenuExtension(context, parent);
     
-    QAction* action = new QAction("Hello, Maxim.");
-
-    qDebug() << "Debug message.";
+    auto* actionMenu = new QMenu;
+    actionMenu->setTitle("QMenu");
     
-    ext.addAction(ext.BuildGroup, action);
+    auto* actionFirst = new QAction("First checkable");
+    auto* actionSecond = new QAction("Second checkable");
+    
+    actionFirst->setCheckable(true);
+    actionSecond->setCheckable(true);
+    
+    actionMenu->addAction(actionFirst);
+    actionMenu->addAction(actionSecond);
+    
+    ext.addAction(ext.ExtensionGroup, actionMenu->menuAction());
+    
+    connect(actionFirst, &QAction::toggled, this, [&](bool checked) {
+        qDebug() << "First action is: " << checked;
+        /* Происходит взрыв, если снять комментарий. Сам по себе он не хочет ставить чекедом его.
+         * Следовательно мы должны запоминать его состояние от вызова к вызову контекстного меню.
+         * И если допустим, при первом запуске KDevelop у нас стоит debug, то setChecked на него.
+         * Вроде бы придает вариативности + адаптивности, но всё равно чувствуется сырость.
+         */
+        
+        //actionFirst->setChecked(checked);
+    });
+    
+    connect(actionSecond, &QAction::toggled, this, [&](bool checked) {
+        qDebug() << "Second action is: " << checked;
+    });
 
     return ext;
 }
